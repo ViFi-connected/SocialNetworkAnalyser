2ed0571f13d3b0c949c1db8854f8b9cad325fb5d
Filip Viduna <viduna.f@gmail.com>
2025-02-02 16:22:26 +0100

    Add project files.

---
+++ SocialNetworkAnalyser/Program.cs
@@ -0,0 +1,40 @@
+using SocialNetworkAnalyser.Client.Pages;
+using SocialNetworkAnalyser.Components;
+
+namespace SocialNetworkAnalyser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents()
+                .AddInteractiveWebAssemblyComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode()
+                .AddInteractiveWebAssemblyRenderMode()
+                .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+
+            app.Run();
+        }
+    }
+}
